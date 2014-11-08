bca59d7067d172618ff7d3bb5ab378f356c1bf61
Jeff Louie <jpl7690@yahoo.com>
2014-11-07 16:35:37 -0800

    working on making a trie

--- js/trie.js
+++ js/trie.js
@@ -1,18 +1,55 @@
 // Defines the node object
 var Node = function(options) {
+  this.value = options.word[options.index];
+  this.word = options.word.substr(0, options.index + 1);
+  this.children = {};
 
+  if(options.index = options.word.length){
+    this.isWord = true;
+  }
+  else{
+    this.isWord = false;
+  }
+
+  this._insert = function(options) {
+    var children = this.children;
+    var letter = options.word[options.index];
+
+    if(!children[letter]){
+      children[letter] = new Node(options);
+    }
+    var node = children[letter];
+    options.index++;
+    var index = options.index;
+    var length = options.word.length;
+
+    // if(index < length){
+    //   node._insert(options);
+    // }
+  };
 };
 
 var Trie = function() {
-  this.head = new Node();
+  var options = {
+    word: '',
+    index: 0
+  }
+  this.head = new Node(options);
+  this.children = this.head.children;
 
   // Inserts string into the Trie.
   this.insert = function(string) {
-
+    var options = {
+      word: string,
+      index: 0
+    }
+    this._insert(this.head, string);
   };
 
   // Recursive function. Helper function for the insert function.
-  this._insert = function() {
+  this._insert = function(node, options) {
+    var children = node.children;
+
 
   };
 
